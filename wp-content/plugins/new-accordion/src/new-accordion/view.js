ccec169bf467dddff205367286c6b27e213c34b2
Milana Cap <milanazzap@gmail.com>
2026-04-15 16:33:03 +0200

    Remove default code: clean up render.php and view.js.

--- wp-content/plugins/new-accordion/src/new-accordion/view.js
+++ wp-content/plugins/new-accordion/src/new-accordion/view.js
@@ -3,26 +3,8 @@
  */
 import { store, getContext } from '@wordpress/interactivity';
 
-const { state } = store( 'gems', {
-	state: {
-		get themeText() {
-			return state.isDark ? state.darkText : state.lightText;
-		},
-	},
-	actions: {
-		toggleOpen() {
-			const context = getContext();
-			context.isOpen = ! context.isOpen;
-		},
-		toggleTheme() {
-			state.isDark = ! state.isDark;
-		},
-	},
-	callbacks: {
-		logIsOpen: () => {
-			const { isOpen } = getContext();
-			// Log the value of `isOpen` each time it changes.
-			console.log( `Is open: ${ isOpen }` );
-		},
-	},
-} );
+const { state } = store('gems', {
+	state: {},
+	actions: {},
+	callbacks: {},
+});
